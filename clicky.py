ff591826df4d7b30e51fe4bbb7fade4da43e6be3
Matt Bierbaum <matt.bierbaum@gmail.com>
2013-01-25 13:42:36 -0500

    all websockets now, static files, favicon

--- clicky.py
+++ clicky.py
@@ -3,43 +3,57 @@ import simplejson
 from gevent.pywsgi import WSGIServer
 from gevent_zeromq import zmq
 from geventwebsocket.handler import WebSocketHandler
-from flask import make_response, Flask, request, render_template
+from flask import make_response, Flask, request, render_template, send_from_directory
+
+# FIXME - include a pool so there is a max number of gets
+# FIXME - spatial data structure for quick loading
 
 context = zmq.Context()
 app = Flask(__name__)
-points = [[0,0],[1,0],[1,1],[0,1],[0,0]]
+points = [[0,0]]
 
-sock = context.socket(zmq.PUB)
-sock.bind("inproc://data")
+qsock = context.socket(zmq.PUB)
+qsock.bind("inproc://data")
 
-@app.route("/datastream")
-def handle_datastream():
+@app.route("/out")
+def handle_out():
     if request.environ.get("wsgi.websocket"):
         ws = request.environ['wsgi.websocket']
-        sock = context.socket(zmq.SUB)
-        sock.setsockopt(zmq.SUBSCRIBE, "")
-        sock.connect('inproc://data')
-        for p in points:
+        ssock = context.socket(zmq.SUB)
+        ssock.setsockopt(zmq.SUBSCRIBE, "")
+        ssock.connect('inproc://data')
+        for p in points[-100:]:
             ws.send(simplejson.dumps(p))
         while True:
-            msg = sock.recv()
+            msg = ssock.recv()
             ws.send(msg)
     return 
 
-@app.route("/pt/<x>/<y>")
-def handle_newpt(x=0, y=0):
-    c = points[-1]
-    points.append([c[0]+int(x),c[1]+int(y)])
-    sock.send(simplejson.dumps(points[-1]))
-    return "ok"
+@app.route("/in")
+def handle_in():
+    if request.environ.get("wsgi.websocket"):
+        ws = request.environ['wsgi.websocket']
+        while True:
+            msg = ws.receive()
+            handle_newpt(msg)
+    return 
+    
+def handle_newpt(cmd=None):
+    mvs = {"u": [0,1], "d": [0,-1], "l": [-1,0], "r": [1,0]}
+    x,y = mvs[cmd]
+    cx,cy = points[-1]
+
+    points.append([cx+x,cy+y])
+    qsock.send(simplejson.dumps(points[-1]))
+    return "."
 
 @app.route("/<path:path>")
 def handle_file(path=None):
-    return render_template(path)
+    return send_from_directory("./static", path)
 
 @app.route("/")
 def handle_index():
-    return render_template("index.html")
+    return send_from_directory("./static", "index.html")
 
 if __name__ == "__main__":
     http_server = WSGIServer(('',8000), app, handler_class=WebSocketHandler)
